8a87dbcc7afe20a3ca1affdec96412d5ffb6804f
manucirne <manucirne@gmail.com>
2019-09-27 14:04:36 -0300

    mux4 errado - colocar when ao no lugar de if

---
+++ componentes/mux4.vhd
@@ -0,0 +1,38 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity mux4 is
+
+    port (
+        a0    : in  std_logic;
+        a1    : in  std_logic;
+		  a2    : in  std_logic;
+		  a3    : in  std_logic;
+        sel   : in  std_logic_vector(1 downto 0);
+        res   : out std_logic
+    );
+end entity;
+
+architecture rtl of mux4 is
+begin
+  -- Para sintetizar lógica combinacional usando um processo,
+  --  todas as entradas do modulo devem aparecer na lista de sensibilidade.
+    process(a0, a1, a2, a3, sel) is
+    begin
+     -- If é uma instrução sequencial que não pode ser usada
+     --  na seção de instruções concorrentes da arquitetura.
+        if(sel='00') then
+            res <= a0;
+        else
+        if(sel='01') then
+            res <= a1;
+			else
+			if(sel='10') then
+            res <= a2;
+			else
+			if(sel='11') then
+            res <= a3;
+				
+        end if;
+    end process;
+end architecture;
