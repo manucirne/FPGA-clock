212649456d636864b277edc1120e08933158761f
manucirne <manucirne@gmail.com>
2019-10-03 18:03:10 -0300

    relogio pra debug com clock mais lento - leds com OPCODE e instrucao - correcao na UC comandos

--- relogio.vhd
+++ relogio.vhd
@@ -7,16 +7,25 @@ ENTITY relogio IS
         sizeio : INTEGER := 8;
 		  sizepc : INTEGER := 16;
 		  sizeinstruction : INTEGER := 28;
-		  sizeopcode : INTEGER := 4
+		  sizeopcode : INTEGER := 4;
+		  
+		  ----====== MARCO
+		  divisor : natural := 50000000
+		  --#######
     );
 
     PORT (
+	 
+		
 			--IN
 			CLOCK_50 : IN std_logic;
 			 SW : in STD_LOGIC_VECTOR(17 downto 0);
 			 KEY : IN STD_LOGIC_VECTOR(3 downto 0);
 		  --OUT
-		  HEX0, HEX1, HEX2, HEX3, HEX4, HEX5 : OUT STD_LOGIC_VECTOR(6 downto 0)
+		  HEX0, HEX1, HEX2, HEX3, HEX4, HEX5 : OUT STD_LOGIC_VECTOR(6 downto 0);
+		  
+		  LEDR : out std_logic_vector(17 downto 0);
+		  LEDG : out std_logic_vector(7 downto 0)
 
     );
 
@@ -27,12 +36,34 @@ ARCHITECTURE rtl OF relogio IS
 	 SIGNAL pcsigaddr: std_logic_vector(15 downto 0);
 	 SIGNAL dataRDsigio, dataWRsigio, addrsigio : std_logic_vector(7 downto 0);
 	 SIGNAL instsigrom : std_logic_vector(27 downto 0);
+	 
+	 
+	 signal tick : std_logic := '0';
+        signal contador : integer range 0 to divisor := 0;
+	 
+	 
 
 BEGIN
 
+
+		process(CLOCK_50)
+        begin
+            if rising_edge(CLOCK_50) then
+                if contador = divisor then
+                    contador <= 0;
+                    tick <= not tick;
+                else
+                    contador <= contador + 1;
+                end if;
+            end if;
+        end process;
+		  
+		  LEDR <= instsigrom(17 downto 0);
+		  LEDG <= pcsigaddr(7 downto 0);
+
     CPURELOGIO : ENTITY work.cpu
         PORT MAP(
-		  clk => CLOCK_50,
+		  clk => tick,
         instruction => instsigrom,
         dataioin => dataRDsigio,
 		  --OUT
@@ -60,7 +91,7 @@ BEGIN
 			  data_write  =>dataWRsigio,
 			  addr  => addrsigio,
 			  write_enable => wrrdiosignal,
-			  clk => CLOCK_50,
+			  clk => tick,
 			  SWitchesin => SW,
 			  KEYbutz => KEY
 				  );
