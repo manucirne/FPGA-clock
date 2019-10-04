27b5c80f1879a3a3aa7e00279287b67550033793
manucirne <manucirne@gmail.com>
2019-10-04 09:48:51 -0300

    com conversor dos displays

--- componentes/io_components/display.vhd
+++ componentes/io_components/display.vhd
@@ -5,20 +5,31 @@ entity display is
     port(
         d7seg            : out std_logic_vector(6 downto 0);
         write_enable    : in  std_logic;
-        data_write      : in std_logic_vector(7 downto 0)
+        data_write      : in std_logic_vector(3 downto 0)
     );
 end display;
 
 architecture comportamento of display is
-    signal display_data : std_logic_vector(6 downto 0);
+    signal data_mem : std_logic_vector(3 downto 0);
+	 signal display_data : std_logic_vector(6 downto 0);
 begin
     
     process(write_enable)
     begin 
         if(write_enable = '1') then
-            display_data <= data_write(6 downto 0);
+            data_mem <= data_write(3 downto 0);
         end if;
     end process;
+	 
+
+conversor : ENTITY work.conversorHex7Seg
+	  PORT MAP(
+	  dadoHex => data_mem,
+	  apaga  => '0',
+	  negativo => '0',
+	  overFlow => '0',
+	  saida7seg => display_data
+  );
 
     d7seg <= display_data;
 
